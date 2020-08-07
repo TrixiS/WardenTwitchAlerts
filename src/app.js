bc99039260468987134c55feba577c4d32982d6a
TrixiS <oficialmorozov@gmail.com>
2020-08-07 18:59:34 +0500

    Use webhooks dir instead of /

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/twitch_callback", (req, res) => {
+app.get("/webhooks/twitch_callback", (req, res) => {
     res.send(req.query["text"]);
     // res.json(json);
     // res.send(json.toString());
