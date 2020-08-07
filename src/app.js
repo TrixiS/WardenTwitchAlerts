bf293526b0fa812f375a7a00b2675346df63e1e2
TrixiS <oficialmorozov@gmail.com>
2020-08-07 21:10:56 +0500

    Add gitignore

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const app = express();
 const secret = JSON.parse(fs.readFileSync("../config.json")).twitch_secret;
 
 app.post("/webhooks/twitch_callback", (req, res) => {
-    if (req.query["hub.secret"] !== secret)
-        return;
+    // if (req.query["hub.secret"] !== secret)
+    //     return;
 
     console.log(req.query.toString());
 });
